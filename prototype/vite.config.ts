6cac1f08d4a9673856ad85bd26c64e09b63a5e9a
李威辰 <eggyy1224@gmail.com>
2025-04-03 22:59:48 +0800

    docs_and_ngrok_config

--- prototype/vite.config.ts
+++ prototype/vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: true, // 允許來自外部網路的連線
+    allowedHosts: [
+      '.ngrok-free.app' // 允許所有 ngrok-free.app 的子網域
+    ],
+  }
 })
